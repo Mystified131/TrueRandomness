cbf0b5cd07715c53b38d510c339c7e1d464914bd
Thomas Park <mystifiedthomas@gmail.com>
2021-03-08 23:39:22 -0600

    Tweaking

---
+++ RealRandTester.py
@@ -0,0 +1,78 @@
+
+import datetime
+import time
+import statistics
+
+def return_digits(num):
+
+    x = len(num)
+
+    outnum = ""
+
+    for ctr in range(x):
+
+        right_now = datetime.datetime.now().isoformat()    
+
+        substr = ""
+
+        for i in right_now:
+            if i.isnumeric():
+                substr += str(i)
+
+        dg = substr[-1:]
+
+        outnum += dg
+
+        time.sleep(.00001)
+
+    aval = (float(outnum) * .001)     
+
+    bval = round(aval, 3)
+
+    return(bval)
+
+def transpose(num, ranm):
+
+    prod = num * ranm
+
+    return(prod)
+
+def random_number(val):
+
+    res1 = return_digits(val)
+
+    numa = int(numin)
+
+    res2 = transpose(numa, res1)
+
+    return res2
+
+print("")
+
+numin = input("Please enter a number to act as a range for the randomness: ")
+
+print("")
+
+numrep = input("How many times would you like to test this?: ")
+
+numr = int(numrep)
+
+alst = []
+
+for ctr in range(numr):
+
+    print(str(ctr))
+
+    #try: 
+    ans = random_number(numin)
+    alst.append(ans)
+
+    #except:
+
+        #print("Recursion Error.")
+
+outval = statistics.mean(alst)
+
+print("The mean is: ", outval)
+
+
